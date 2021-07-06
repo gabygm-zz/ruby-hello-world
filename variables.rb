f481e4f93d599e49c49bcb42c0a61a75b9978330
Gaby G <paogaby0803@gmail.com>
2021-07-05 22:41:23 -0500

    variables

---
+++ variables.rb
@@ -0,0 +1,14 @@
+name = "Gaby G"
+address = ["Loja", "Taquil"]
+puts name
+p address
+#console
+last_name = gets.chomp
+p last_name
+if last_name == "gaby"
+    true
+else
+    false
+end    
+#scope
+
